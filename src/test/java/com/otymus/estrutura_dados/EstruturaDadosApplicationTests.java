c2fcf2db506cd5b31fec3ff193a9bb253df33592
Fábio <otymus27@gmail.com>
2025-11-17 15:34:58 -0300

    Estrutura inicial

---
+++ src/test/java/com/otymus/estrutura_dados/EstruturaDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otymus.estrutura_dados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstruturaDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
